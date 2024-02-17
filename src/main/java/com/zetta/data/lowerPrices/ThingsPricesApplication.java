16034227d5d03b411800f03bd7a691ee787644d5
Ployola <ployola@hiper.com.pe>
2024-02-16 23:13:17 -0500

    Init project

---
+++ src/main/java/com/zetta/data/lowerPrices/ThingsPricesApplication.java
@@ -0,0 +1,13 @@
+package com.zetta.data.lowerPrices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThingsPricesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThingsPricesApplication.class, args);
+	}
+
+}
